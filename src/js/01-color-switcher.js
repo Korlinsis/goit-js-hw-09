2a4c51bff6f2330df553e473423d7930d9518dd8
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-01-30 15:34:34 +0200

    Add task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+
+function onStartClick () {
+    startButton.disabled = true;
+    intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopClick () {
+    startButton.disabled = false;
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
