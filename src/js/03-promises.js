ad326c9fa2d6e64f86a0c05fa04b50654735d968
Ivan Kiziuk <ivanmnksos@gmail.com>
2023-01-31 03:15:01 +0200

    Add 03 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formForPromiseGeneration = document.querySelector('.form');
+const {elements: { delay, step, amount }} = formForPromiseGeneration;
+
+formForPromiseGeneration.addEventListener('submit', createPromises);
+
+function createPromises(e) {
+    e.preventDefault();
+    for (let i = 0; i < amount.value; i += 1) {
+    let timerDelay = (i * step.value) + Number(delay.value);
+    createPromise(i + 1, timerDelay);
+    }
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        } else {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }
+    }, delay);
 }
